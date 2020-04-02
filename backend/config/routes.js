02165b6601af15fe6688ad4cb6cc1991f5558dde
David <davidtavares98@gmail.com>
2020-04-02 16:38:29 -0300

    Update

---
+++ backend/config/routes.js
@@ -0,0 +1,33 @@
+module.exports = app => {
+  app
+    .route("/users")
+    .post(app.api.user.save)
+    .get(app.api.user.get);
+
+  app
+    .route("/users/:id")
+    .put(app.api.user.save)
+    .get(app.api.user.getById);
+  app
+    .route("/categories")
+    .get(app.api.category.get)
+    .post(app.api.category.save);
+
+    app.route("/categories/tree")
+        .get(app.api.category.getTree)
+
+  /* Cuidado com a ordem! */
+  app
+    .route("/categories/:id")
+    .get(app.api.category.getById)
+    .put(app.api.category.save)
+    .delete(app.api.category.remove);
+  
+  app.route('/articles')
+    .get(app.api.article.get)
+    .post(app.api.article.save)
+  app.route('/articles/:id')
+    .get(app.api.article.getById)
+    .put(app.api.article.save)
+    .delete(app.api.article.remove)
+};
