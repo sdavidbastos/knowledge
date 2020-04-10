6bafa2c542a97841957ddf7787498cc9da9da8df
sdavidbastos <davidtavares98@gmail.com>
2020-04-10 20:58:15 -0300

    Componentes: Article, Category e User

--- frontend/src/components/admin/AdminPages.vue
+++ frontend/src/components/admin/AdminPages.vue
@@ -1,14 +1,32 @@
 <template>
   <div class="admin-pages">
       <PageTitle icon="fa fa-cogs" main="Administração do Sistema" sub="Cadastros & Cia"/>
+      <div class="admin-pages-tables">
+        <b-card no-body>
+          <b-tabs card>
+            <b-tab title="Artigos" active>
+              <ArticleAdmin />
+            </b-tab>
+            <b-tab title="Categorias">
+                <CategoryAdmin />
+            </b-tab>
+            <b-tab title="Usuarios">
+              <UserAdmin />
+            </b-tab>
+          </b-tabs>
+        </b-card>
+      </div>
   </div>
 </template>
 
 <script>
 import PageTitle from '../template/PageTitle'
+import ArticleAdmin from './ArticleAdmin'
+import CategoryAdmin from './CategoryAdmin'
+import UserAdmin from './UserAdmin'
 export default {
     name: 'AdminPages',
-    components: { PageTitle }
+    components: { PageTitle, ArticleAdmin, CategoryAdmin, UserAdmin }
 }
 </script>
 
---
+++ frontend/src/components/admin/UserAdmin.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="user-admin">
+      <h1>Usuario Componente</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"UserAdmin"
+}
+</script>
+
+<style>
+
+</style>
