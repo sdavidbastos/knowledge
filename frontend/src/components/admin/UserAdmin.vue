404ef1983cf5b9eee120516aff47ec31b6ca6c38
sdavidbastos <davidtavares98@gmail.com>
2020-04-10 21:16:51 -0300

    Tabela de usuarios

--- frontend/src/components/admin/UserAdmin.vue
+++ frontend/src/components/admin/UserAdmin.vue
@@ -1,15 +1,47 @@
 <template>
   <div class="user-admin">
-      <h1>Usuario Componente</h1>
+    <b-table hover striped :items="users" :fields="fields"></b-table>
   </div>
 </template>
 
 <script>
+import { baseApiUrl } from "@/global";
+import axios from "axios";
+
 export default {
-    name:"UserAdmin"
-}
+  name: "UserAdmin",
+  data: function() {
+    return {
+      mode: "save",
+      user: {},
+      users: [],
+      fields: [
+        { key: "id", label: "Codigo", sortable: true },
+        { key: "name", label: "Nome", sortable: true },
+        { key: "email", label: "E-mail", sortable: true },
+        {
+          key: "admin",
+          label: "Administrador",
+          sortable: true,
+          formatter: value => (value ? "sim" : "não")
+        },
+        { key: "actions", label: "Ações" }
+      ]
+    };
+  },
+  methods: {
+    loadUsers() {
+      const url = `${baseApiUrl}/users`;
+      axios.get(url).then(response => {
+        this.users = response.data;
+      });
+    }
+  },
+  mounted() {
+    this.loadUsers();
+  }
+};
 </script>
 
 <style>
-
 </style>
